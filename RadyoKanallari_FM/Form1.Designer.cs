7d3d4081b5f27c2d6c1339214bb71a65a5b80eba
Rabia Aydemir <aydemirabia@gmail.com>
2022-08-06 21:47:07 +0300

    CSharp- Radyo Frekanslarına Canlı Bağlanma
    C# - MMS adresleri kullanılarak radyo frekanslarına canlı olarak bağlanılmıştır.
    8 adet canlı radyo kanalı kullanılmıştır.

---
+++ RadyoKanallari_FM/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace RadyoKanallari_FM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.brnCikis = new System.Windows.Forms.Button();
+            this.btnBestFm = new System.Windows.Forms.Button();
+            this.btnJoyTurk = new System.Windows.Forms.Button();
+            this.btnPowerFm = new System.Windows.Forms.Button();
+            this.btnMetroFm = new System.Windows.Forms.Button();
+            this.btnTRT = new System.Windows.Forms.Button();
+            this.btnIstanbul = new System.Windows.Forms.Button();
+            this.btnFenomen = new System.Windows.Forms.Button();
+            this.btnSlowTurk = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(321, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(323, 456);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // brnCikis
+            // 
+            this.brnCikis.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("brnCikis.BackgroundImage")));
+            this.brnCikis.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.brnCikis.Location = new System.Drawing.Point(427, 474);
+            this.brnCikis.Name = "brnCikis";
+            this.brnCikis.Size = new System.Drawing.Size(113, 77);
+            this.brnCikis.TabIndex = 1;
+            this.brnCikis.UseVisualStyleBackColor = true;
+            this.brnCikis.Click += new System.EventHandler(this.brnCikis_Click);
+            // 
+            // btnBestFm
+            // 
+            this.btnBestFm.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBestFm.BackgroundImage")));
+            this.btnBestFm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBestFm.Location = new System.Drawing.Point(12, 12);
+            this.btnBestFm.Name = "btnBestFm";
+            this.btnBestFm.Size = new System.Drawing.Size(143, 119);
+            this.btnBestFm.TabIndex = 2;
+            this.btnBestFm.UseVisualStyleBackColor = true;
+            this.btnBestFm.Click += new System.EventHandler(this.btnBestFm_Click);
+            // 
+            // btnJoyTurk
+            // 
+            this.btnJoyTurk.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnJoyTurk.BackgroundImage")));
+            this.btnJoyTurk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnJoyTurk.Location = new System.Drawing.Point(172, 12);
+            this.btnJoyTurk.Name = "btnJoyTurk";
+            this.btnJoyTurk.Size = new System.Drawing.Size(143, 119);
+            this.btnJoyTurk.TabIndex = 3;
+            this.btnJoyTurk.UseVisualStyleBackColor = true;
+            this.btnJoyTurk.Click += new System.EventHandler(this.btnJoyTurk_Click);
+            // 
+            // btnPowerFm
+            // 
+            this.btnPowerFm.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnPowerFm.BackgroundImage")));
+            this.btnPowerFm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPowerFm.Location = new System.Drawing.Point(172, 150);
+            this.btnPowerFm.Name = "btnPowerFm";
+            this.btnPowerFm.Size = new System.Drawing.Size(143, 119);
+            this.btnPowerFm.TabIndex = 5;
+            this.btnPowerFm.UseVisualStyleBackColor = true;
+            this.btnPowerFm.Click += new System.EventHandler(this.btnPowerFm_Click);
+            // 
+            // btnMetroFm
+            // 
+            this.btnMetroFm.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnMetroFm.BackgroundImage")));
+            this.btnMetroFm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnMetroFm.Location = new System.Drawing.Point(12, 150);
+            this.btnMetroFm.Name = "btnMetroFm";
+            this.btnMetroFm.Size = new System.Drawing.Size(143, 119);
+            this.btnMetroFm.TabIndex = 4;
+            this.btnMetroFm.UseVisualStyleBackColor = true;
+            this.btnMetroFm.Click += new System.EventHandler(this.btnMetroFm_Click);
+            // 
+            // btnTRT
+            // 
+            this.btnTRT.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnTRT.BackgroundImage")));
+            this.btnTRT.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnTRT.Location = new System.Drawing.Point(172, 288);
+            this.btnTRT.Name = "btnTRT";
+            this.btnTRT.Size = new System.Drawing.Size(143, 119);
+            this.btnTRT.TabIndex = 7;
+            this.btnTRT.UseVisualStyleBackColor = true;
+            this.btnTRT.Click += new System.EventHandler(this.btnTRT_Click);
+            // 
+            // btnIstanbul
+            // 
+            this.btnIstanbul.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnIstanbul.BackgroundImage")));
+            this.btnIstanbul.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnIstanbul.Location = new System.Drawing.Point(12, 288);
+            this.btnIstanbul.Name = "btnIstanbul";
+            this.btnIstanbul.Size = new System.Drawing.Size(143, 119);
+            this.btnIstanbul.TabIndex = 6;
+            this.btnIstanbul.UseVisualStyleBackColor = true;
+            this.btnIstanbul.Click += new System.EventHandler(this.btnIstanbul_Click);
+            // 
+            // btnFenomen
+            // 
+            this.btnFenomen.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnFenomen.BackgroundImage")));
+            this.btnFenomen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnFenomen.Location = new System.Drawing.Point(172, 425);
+            this.btnFenomen.Name = "btnFenomen";
+            this.btnFenomen.Size = new System.Drawing.Size(143, 119);
+            this.btnFenomen.TabIndex = 9;
+            this.btnFenomen.UseVisualStyleBackColor = true;
+            this.btnFenomen.Click += new System.EventHandler(this.btnFenomen_Click);
+            // 
+            // btnSlowTurk
+            // 
+            this.btnSlowTurk.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSlowTurk.BackgroundImage")));
+            this.btnSlowTurk.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSlowTurk.Location = new System.Drawing.Point(12, 425);
+            this.btnSlowTurk.Name = "btnSlowTurk";
+            this.btnSlowTurk.Size = new System.Drawing.Size(143, 119);
+            this.btnSlowTurk.TabIndex = 8;
+            this.btnSlowTurk.UseVisualStyleBackColor = true;
+            this.btnSlowTurk.Click += new System.EventHandler(this.btnSlowTurk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(658, 563);
+            this.Controls.Add(this.btnFenomen);
+            this.Controls.Add(this.btnSlowTurk);
+            this.Controls.Add(this.btnTRT);
+            this.Controls.Add(this.btnIstanbul);
+            this.Controls.Add(this.btnPowerFm);
+            this.Controls.Add(this.btnMetroFm);
+            this.Controls.Add(this.btnJoyTurk);
+            this.Controls.Add(this.btnBestFm);
+            this.Controls.Add(this.brnCikis);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "RADYO FM";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button brnCikis;
+        private System.Windows.Forms.Button btnBestFm;
+        private System.Windows.Forms.Button btnJoyTurk;
+        private System.Windows.Forms.Button btnPowerFm;
+        private System.Windows.Forms.Button btnMetroFm;
+        private System.Windows.Forms.Button btnTRT;
+        private System.Windows.Forms.Button btnIstanbul;
+        private System.Windows.Forms.Button btnFenomen;
+        private System.Windows.Forms.Button btnSlowTurk;
+    }
+}
+
---
+++ RadyoKanallari_FM/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadyoKanallari_FM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBestFm_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://46.20.7.125/listen.pls";
+        }
+
+        private void btnJoyTurk_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://provisioning.streamtheworld.com/pls/JOY_TURKAAC.pls";
+        }
+
+        private void btnMetroFm_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://provisioning.streamtheworld.com/pls/METRO_FMAAC.pls";
+        }
+
+        private void btnPowerFm_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://powerfm.listenpowerapp.com/powerfm/mpeg/icecast.audio";
+        }
+
+        private void btnIstanbul_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://45.32.154.169:9300/;";
+        }
+
+        private void btnTRT_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://trtcanlifm-lh.akamaihd.net/i/TRTFM_1@181846/master.m3u8";
+        }
+
+        private void btnSlowTurk_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://r3.rocketcdn.com/slowturk/abr/playlist.m3u8";
+        }
+
+        private void btnFenomen_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://fenomen.listenfenomen.com/fenomen/128/icecast.audio";
+        }
+
+        private void brnCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
